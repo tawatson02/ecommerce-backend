7f4d287b80313d4bf671ad43ae0602593d000747
tawatson02 <ta_watson@live.com>
2024-07-10 22:14:01 -0500

    I specified the models for the Category, Product, ProductTag, and Tag files. Also, I linked the files with the foreignKeys in the Index.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,22 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: ProductTag
+});
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: ProductTag
+});
 module.exports = {
   Product,
   Category,
